4c95f3e05a6d9534b718d0fb20c5fab326b471bb
Kawan <kawanrandoli@gmail.com>
2021-04-12 18:08:39 -0300

    Atualização caesar.c

--- caesar.c
+++ caesar.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <ctype.h> //For isalpha function
-#include <string.h> //For strlen ffunction
+#include <ctype.h> //For isalpha, isupper, islower function
+#include <string.h> //For strlen function
 
 int main(void) {
     char plainText[20], cipherText[20];
     int key, cipherKey, keyIsValid, plainIsValid, inputTextSize, i, ii;
-    float n;
     char alphabet[2][26] = {
         {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},
         {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
     };
     //Initializing variables
-    key = cipherKey = keyIsValid = plainIsValid = inputTextSize = n = 0;
+    key = cipherKey = keyIsValid = plainIsValid = inputTextSize = 0;
     //Getting Key input and validating
     printf("Key: ");
     while (keyIsValid != 1) {
@@ -29,7 +28,8 @@ int main(void) {
     fgets(plainText, sizeof(plainText), stdin);
     inputTextSize = strlen(plainText) - 1;
     for (i = 0; i < inputTextSize; i++) {
-        if (isalpha(plainText[i]) != 0) {
+        if (isalpha(plainText[i]) != 0 || isspace(plainText[i]) != 0 || ispunct(plainText[i]) != 0) {
+            // empty
         } else {
             printf("Invalid input\n");
             break;
@@ -37,14 +37,15 @@ int main(void) {
     }
     // Obtaining the Cipher Key
     for (i = 0; i < inputTextSize; i++) {
-        printf("I: %d", i);
         if (isupper(plainText[i]) != 0) {
             ii = 0;
             do {
                 cipherKey = (ii + key) % 26;
-                cipherText[i] = alphabet[1][cipherKey];
+                cipherText[i] = alphabet[0][cipherKey];
                 cipherKey = 0;                
                 ii++;
+                cipherText[i++] = '\0';
+                printf("%d", i);
             } while (plainText[i] != alphabet[0][ii]);
         } else if (islower(plainText[i]) != 0) {
             ii = 0;
@@ -53,18 +54,14 @@ int main(void) {
                 cipherText[i] = alphabet[1][cipherKey];
                 cipherKey = 0;                
                 ii++;
-            } while (plainText[i] != alphabet[0][ii]);
-            // for (ii = 0; (plainText[i] != alphabet[1][ii]); ii++) {
-            //     cipherKey = (ii + key) % 26;
-            //     cipherText[i] = alphabet[1][cipherKey];
-            //     cipherKey = 0;                
-            // }
+                cipherText[i++] = '\0';
+            } while (plainText[i] != alphabet[1][ii]);
         } else {
             printf("ERROR");
         }
     }
-    printf("Plain Text: %s", plainText);
-    printf("Cipher Text: %s", cipherText);
+    printf("\nPlain Text: %s\n", plainText);
+    printf("Cipher Text: %s\n", cipherText);
 }
 /*
     ci = (pi + k) % 26
